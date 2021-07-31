a1f7bfdc7fa893f78f8b3c2804afe1ad05313bfd
Kathy Nguyen <kathyn262@gmail.com>
2021-07-30 17:54:25 -0700

    added navbar

---
+++ src/Navigation/index.js
@@ -0,0 +1,30 @@
+import { NavLink } from "react-router-dom";
+import React from "react";
+import "./Navigation.css";
+
+function Navigation() {
+  return (
+    <div className="row Nav-row">
+      <div className="col Nav-col">
+        <nav className="navbar navbar-expand-lg">
+          <NavLink className="navbar-brand" id="Nav-brand-name" to="/">bmorize</NavLink>
+          <button className="navbar-toggler" type="button" data-toggle="collapse"
+            data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
+            aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="/tutorial">Tutorial</NavLink></li>
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="/game">Play Now</NavLink></li>
+              </ul>
+          </div>
+        </nav>
+      </div>
+    </div>
+  );
+}
+
+export default Navigation;
