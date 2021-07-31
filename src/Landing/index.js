c3e37db96266c5e11256c3574111d114a06f31d9
Kathy Nguyen <kathyn262@gmail.com>
2021-07-31 15:55:27 -0700

    started to populate landing pg

--- src/Landing/index.js
+++ src/Landing/index.js
@@ -9,61 +9,67 @@ function Landing() {
     <React.Fragment>
       <div className="row Landing-row">
         <div className="col my-auto">
-          <h1>Memorize with bmorize</h1>
-          <p>something something
-          other headers are: practice memorization, train your memory
-          </p>
-          <a className="btn btn-main" href="/tutorial" role="button">Tutorial</a>
-          <a className="btn btn-main btn-play-now" href="/game" role="button">Play Now</a>
+          <h1 className="Landing-header">Memorize with bmorize</h1>
+          <p className="Landing-body-text">Level up your memory today.</p>
+          <a className="btn btn-main" href="/tutorial" role="button">
+            Tutorial
+          </a>
+          <a className="btn btn-main btn-play-now" href="/game" role="button">
+            Play Now
+          </a>
         </div>
         <div className="col">
           <img className="Landing-mascot" src={mascot} alt="mascot"></img>
         </div>
       </div>
       <div className="row Landing-row">
+        <div className="col">screenshot here</div>
         <div className="col">
-          screenshot here
-        </div>
-        <div className="col">
-          <h1>Select a difficulty</h1>
-          <p>
-            Some days you might be up for a challenge, some days you might not be.
+          <h1 className="Landing-header">Select a difficulty</h1>
+          <p className="Landing-body-text">
+            Some days you might be up for a challenge, some days you might not
+            be.
+          </p>
+          <p className="Landing-body-text">
             You can adjust the difficulty of the game with ease.
           </p>
         </div>
       </div>
       <div className="row Landing-row">
         <div className="col">
-          <h1>Win by matching all cards</h1>
-          <p>
-            idk what is a good header rn tbh
+          <h1 className="Landing-header">Win by matching all cards</h1>
+          <p className="Landing-body-text">No distractions.</p>
+          <p className="Landing-body-text">One clear goal.</p>
+        </div>
+        <div className="col">screenshot here</div>
+      </div>
+      <div className="row Landing-row">
+        <div className="col">
+          <h1 className="Landing-header">Meet the Engineers</h1>
+          <img
+            className="Landing-engineer-icon"
+            src="https://kathyn138.github.io/assets/custom/about-pic.png"
+            alt=""
+          ></img>
+          <h2 className="Landing-header">Kathy Nguyen</h2>
+          <p className="Landing-body-text">
+            A software engineer who is passionate about creating things that
+            help people.
           </p>
-        </div>
-        <div className="col">
-          screenshot here
-        </div>
-      </div>
-      <div className="row Landing-row">
-        <div className="col">
-          screenshot here
-        </div>
-      </div>
-      <div className="row Landing-row">
-        <div className="col">
-          <h1>Meet the Engineers</h1>
-        </div>
-      </div>
-      <div className="row Landing-row">
-        <div className="col">
-          <img className="Landing-engineer-icon" src="https://cdn.discordapp.com/attachments/729097963095457844/839648505005342730/813819507658719232.png" alt="engineer1"></img>
-          <h1>Kathy Nguyen</h1>
-          <p>person1 bio</p>
           <p className="Landing-engineer-links">
             <a href="https://github.com/kathyn138">
-              <img className="Landing-social-icon" src={githubIcon} alt=""></img>
+              <img
+                className="Landing-social-icon"
+                src={githubIcon}
+                alt=""
+              ></img>
             </a>
             <a href="https://www.linkedin.com/in/kathy-n-709a5">
-              <img className="Landing-social-icon" src={linkedinIcon} alt=""></img>
+              <img
+                className="Landing-social-icon"
+                src={linkedinIcon}
+                alt=""
+              ></img>
             </a>
           </p>
         </div>
