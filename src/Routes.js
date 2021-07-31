a96e063854433c3c98e513ea402d5f0efc1ef5a2
Kathy Nguyen <kathyn262@gmail.com>
2021-07-30 17:53:08 -0700

    removed unused components

--- src/Routes.js
+++ src/Routes.js
@@ -2,11 +2,6 @@ import React from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 import Game from "./Game";
 import Home from "./Home";
-import Leaderboard from "./Leaderboard";
-import LoginForm from "./LoginForm";
-import PrivateRoute from "./PrivateRoute";
-import Profile from "./Profile";
-import RegisterForm from "./RegisterForm";
 import Tutorial from "./Tutorial";
 
 function Routes() {
@@ -20,25 +15,9 @@ function Routes() {
         <Tutorial />
       </Route>
 
-      <Route exact path="/login">
-        <LoginForm />
-      </Route>
-
-      <Route exact path="/register">
-        <RegisterForm />
-      </Route>
-
-      <Route exact path="/leaderboard">
-        <Leaderboard />
-      </Route>
-
       <Route exact path="/game">
         <Game />
       </Route>
-
-      <PrivateRoute exact path="/profile">
-        <Profile />
-      </PrivateRoute>
 
       <Redirect to="/"></Redirect>
     </Switch>
