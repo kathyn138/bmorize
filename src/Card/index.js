f68b9e3794d69ec06d36c31bdb9d9fe5211f5f1f
Kathy Nguyen <kathyn262@gmail.com>
2021-07-30 23:40:55 -0700

    added card img

--- src/Card/index.js
+++ src/Card/index.js
@@ -14,18 +14,17 @@ function Card({ card, flipStatus, matchStatus, handleCardFlip, }) {
     }
   }
 
-  const flipStyling = flipStatus || matchStatus ? 'is-flipped' : null;
+  const flipStyling = (flipStatus || matchStatus) && 'is-flipped';
 
   return (
-    <div className="col">
-
+    <div className="col Card-col">
       <div className="Card-scene">
         <div className={`Card-flip-card ${flipStyling}`} onClick={handleClick}>
           <div className="Card-front">
-            {card[1]}
+            {card.value}
           </div>
           <div className="Card-back">
-            "back"
+            <img className="Card-img" src={card.image} alt={card.value}></img>
         </div>
         </div>
       </div>
