895327cbca003641ae67084a877305f1a7bd4ead
Kathy Nguyen <kathyn262@gmail.com>
2021-07-31 16:38:05 -0700

    added card front img

--- src/Card/index.js
+++ src/Card/index.js
@@ -1,31 +1,36 @@
-import React from 'react';
+import React from "react";
+import mascot from "../assets/bmo-card-front.png";
 import "./Card.css";
 
-function Card({ card, flipStatus, matchStatus, handleCardFlip, }) {
+function Card({ card, flipStatus, matchStatus, handleCardFlip }) {
   function handleClick() {
     // only flip card if it's not matched yet
     if (!matchStatus) {
       // if card is already flipped, unflip
       if (flipStatus) {
-        handleCardFlip('unflip', card);
+        handleCardFlip("unflip", card);
       } else {
-        handleCardFlip('flip', card);
+        handleCardFlip("flip", card);
       }
     }
   }
 
-  const flipStyling = (flipStatus || matchStatus) && 'is-flipped';
+  const flipStyling = (flipStatus || matchStatus) && "is-flipped";
 
   return (
     <div className="col Card-col">
       <div className="Card-scene">
         <div className={`Card-flip-card ${flipStyling}`} onClick={handleClick}>
           <div className="Card-front">
-            {card.value}
+            <img className="Card-front-img" src={mascot} alt=""></img>
           </div>
           <div className="Card-back">
-            <img className="Card-img" src={card.image} alt={card.value}></img>
-        </div>
+            <img
+              className="Card-back-img"
+              src={card.image}
+              alt={card.value}
+            ></img>
+          </div>
         </div>
       </div>
     </div>
